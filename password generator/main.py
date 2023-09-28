ba4fc2bc1f718d3d6717b4c383bdd72129b13a3b
Shreya2408 <85797705+Shreya2408@users.noreply.github.com>
2023-09-28 18:48:06 +0530

    main.py

--- password generator/main.py
+++ password generator/main.py
@@ -1,11 +1,10 @@
 import string
 import random
 
-if __name__ == "__main__":
-    s1 = string.ascii_lowercase
-    s2 = string.ascii_uppercase
-    s3 = string.punctuation
-    s4 = string.digits
+s1 = string.ascii_lowercase
+s2 = string.ascii_uppercase
+s3 = string.punctuation
+s4 = string.digits
     
 print("------------Password Generator-------------")
 pas = int(input("Enter the length of the password : "))

1121a808656aa5152d94e6552e4c8f2e7685c366
Shreya2408 <85797705+Shreya2408@users.noreply.github.com>
2023-09-28 18:46:24 +0530

    main.py

--- password generator/main.py
+++ password generator/main.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
 print("------------Password Generator-------------")
 pas = int(input("Enter the length of the password : "))
 
-s = []
+s = []      
 s.extend(s1)
 s.extend(s2)
 s.extend(s3)
 s.extend(s4)
 
-random.shuffle(s)
+random.shuffle(s)  #used to shuffle the list elements
 
-print("Your Password is : ", "".join(s[0:pas]))
+print("Your Password is : ", "".join(s[0:pas]))  # join is used to join the elements removing the quotes
 
